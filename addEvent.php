977a064d3deef846b985bfc581428d332d533138
sagar <sagar.choudhary@innoraft.com>
2015-03-19 16:17:06 +0530

    refs #9864  add event working

--- addEvent.php
+++ addEvent.php
@@ -2,19 +2,19 @@
   session_start();
   $uid=$_SESSION['uid'];
   $ename=$_POST["ename"];
-  $eimg=$_POST["eimg"];
+
   $edescription=$_POST["edescription"];
   $owner=$_POST["owner"];
   $dbhost = 'localhost:3036';
   $dbuser = 'root';
   $dbpass = '123456';
+
   $conn = mysql_connect($dbhost, $dbuser, $dbpass);
   if(! $conn )
   {
-  die('Could not connect: ' . mysql_error());
+    die('Could not connect: ' . mysql_error());
   }
   $sql_tax = 'select * from taxonomy';
-
   mysql_select_db('events');
   $retval = mysql_query( $sql_tax, $conn );
   if(! $retval )
@@ -22,11 +22,13 @@
     die('Could not enter data: ' . mysql_error());
   }
   $all_results_tax = array();
-    while ($result = mysql_fetch_assoc($retval)){
+  while ($result = mysql_fetch_assoc($retval))
+  {
     $all_results_tax[] = $result;
-    }
-    $sql_user = 'select name from user where role="user"';
+  }
 
+
+  $sql_user = 'select name from user where role="user"';
   mysql_select_db('events');
   $retval = mysql_query( $sql_user, $conn );
   if(! $retval )
@@ -40,20 +42,49 @@
 
   if(isset($_POST["add"]))
   {
-  
-	
 
-	$sql = 'insert into user (name,email,password,role) values ("'.$name.'","'.$email.'","'.$password.'","'.$role.'")';
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["eimg"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+    if (file_exists($target_file)) {
+      echo "Sorry, file already exists.";
+      $uploadOk = 0;
+    }
 
-	mysql_select_db('events');
-	$retval = mysql_query( $sql, $conn );
-	if(! $retval )
-	{
-  	die('Could not enter data: ' . mysql_error());
-	}
+    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif" ) 
+    {
+      echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+      $uploadOk = 0;
+    }
+
+    if ($uploadOk == 0) {
+      echo "Sorry, your file was not uploaded.";
+      // if everything is ok, try to upload file
+    } 
+    else {
+        if (move_uploaded_file($_FILES["eimg"]["tmp_name"], $target_file)) {
+          echo "The file ". basename( $_FILES["eimg"]["name"]). " has been uploaded.";
+          $img_name=basename( $_FILES["eimg"]["name"]);
+        } 
+        else {
+        echo "Sorry, there was an error uploading your file.";
+        }
+    }
+    $sql = 'insert into event (uid,ename,eimg,edescription,owner) values ("'.$uid.'","'.$ename.'","'.$img_name.'","'.$edescription.'","'.$owner.'")';
+
+	 mysql_select_db('events');
+	 $retval = mysql_query( $sql, $conn );
+	 if(! $retval )
+	 {
+  	 die('Could not enter data: ' . mysql_error());
+	 }
 	
-	mysql_close($conn);
-  }	
+	 mysql_close($conn);
+   header('location:landing.php');
+  }
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -62,7 +93,7 @@
 </head>
 <body>
 <h1>Add events</h1>
-	<form action="<?php $_PHP_SELF ?>" method="post">
+	<form action="<?php $_PHP_SELF ?>" method="post" enctype="multipart/form-data">
   Event Name: <input type="text" name="ename" /><br><br><br>
   Upload Image: <input type="file" name="eimg" /><br><br><br>
   Event Description:<input type="textarea" name="edescription"><br><br><br>
@@ -72,7 +103,7 @@
     foreach ($all_results_tax as $key => $value) { ?>
     
        
-      <option value="<?php echo($all_results_tax[$key]['name']); ?>"><?php echo($all_results_tax[$key]['name']); ?></option>
+      <option value="<?php echo($all_results_tax[$key]['tid']); ?>"><?php echo($all_results_tax[$key]['name']); ?></option>
            
    <?php  } ?>
 
